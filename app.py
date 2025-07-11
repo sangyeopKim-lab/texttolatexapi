3eb9053569c0435081be48a31aca4e3125f70275
sangyeopKim-lab <yeop1116@gmail.com>
2025-07-11 19:35:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from sympy import sympify, latex
+import os
 
 app = Flask(__name__)
 
@@ -15,4 +16,5 @@ def convert_text_to_latex():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))  
+    app.run(host="0.0.0.0", port=port)        
